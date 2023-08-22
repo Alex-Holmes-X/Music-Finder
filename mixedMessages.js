70e058d53cfdb748b8b0264430a6332bba63027b
Alex Holmes <alex_j_holmes@outlook.com>
2023-08-22 21:30:41 +0100

    first commit

--- mixedMessages.js
+++ mixedMessages.js
@@ -7,4 +7,17 @@ const genre = ['rock','pop','blues','alternative','soul','easy listening']
 
 const mood = ['happy','sad','fast','slow','chill','spiritual','off beat']
 
+// console.log(decade)
 
+
+const musicFinder = () => {
+    let randomYear = decade[Math.floor(Math.random() * decade.length)];
+    console.log(randomYear)
+    let randomGenre = genre[Math.floor(Math.random() * genre.length)];
+    console.log(randomGenre)
+    let randomMood = mood[Math.floor(Math.random() * mood.length)];
+    console.log(randomMood)
+    console.log("Im a Music Finder, i will find something for you to listen to!!")
+} 
+
+musicFinder()
