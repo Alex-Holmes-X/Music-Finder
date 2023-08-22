e153401588b9508848e78b424082f83222bfed8d
Alex Holmes <alex_j_holmes@outlook.com>
2023-08-22 21:08:02 +0100

    Updated Random array generators

--- mixedMessages.js
+++ mixedMessages.js
@@ -1,1 +1,10 @@
-testing
+// Randomly Generate a custom car for a customer
+
+
+const decade = ['60s','70s','80s','90s','00s','10s']
+
+const genre = ['rock','pop','blues','alternative','soul','easy listening']
+
+const mood = ['happy','sad','fast','slow','chill','spiritual','off beat']
+
+
