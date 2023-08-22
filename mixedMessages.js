78748e8a16d8abc5ae6ba23d7c201497436bcdd8
Alex Holmes <alex_j_holmes@outlook.com>
2023-08-22 21:42:05 +0100

    Final draft

--- mixedMessages.js
+++ mixedMessages.js
@@ -3,21 +3,22 @@
 
 const decade = ['60s','70s','80s','90s','00s','10s']
 
-const genre = ['rock','pop','blues','alternative','soul','easy listening']
+const genre = ['Rock','Pop','Blues','Alternative','Soul','Easy Listening']
 
-const mood = ['happy','sad','fast','slow','chill','spiritual','off beat']
+const mood = ['Happy','Sad','Fast','Slow','Chill','Spiritual','Off Beat']
 
 // console.log(decade)
 
 
 const musicFinder = () => {
     let randomYear = decade[Math.floor(Math.random() * decade.length)];
-    console.log(randomYear)
+    // console.log(randomYear)
     let randomGenre = genre[Math.floor(Math.random() * genre.length)];
-    console.log(randomGenre)
+    // console.log(randomGenre)
     let randomMood = mood[Math.floor(Math.random() * mood.length)];
-    console.log(randomMood)
+    // console.log(randomMood)
     console.log("Im a Music Finder, i will find something for you to listen to!!")
+    console.log(`I think you should listen to ${randomMood}, ${randomGenre} music from the ${randomYear}`)
 } 
 
 musicFinder()
